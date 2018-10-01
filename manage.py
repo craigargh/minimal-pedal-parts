c0563b55100391000c01bd46825cce15c9075120
craigargh <craigargh@gmail.com>
2018-10-01 21:27:23 +0100

    Add first pass at manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,20 @@
-pass
+from pedalparts import part, pedal
+
+
+def create_pedal():
+    name = input('Enter the pedal name: ')
+    print('Enter the parts (press enter twice to finish):')
+
+    parts = []
+
+    raw_part = input('')
+
+    while raw_part != '':
+        new_part = part.parse(raw_part)
+        parts.append(new_part)
+
+        raw_part = input('')
+
+    new_pedal = pedal.create(name, parts)
+
+    pedal.save(new_pedal)
