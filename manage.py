5abe38ac87fb721dfcbefa82d42801d624e82ccf
craigargh <craigargh@gmail.com>
2018-10-06 14:52:05 +0100

    Add resistors to part

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from pedalparts import parttools, pedaltools
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', choices=['add_pedal', 'add_part', 'missing'])
+    parser.add_argument('mode', choices=['add_pedal', 'add_part', 'add_parts', 'missing'])
 
     parser.add_argument('--name', required=False)
     parser.add_argument('--file', required=False)
@@ -28,6 +28,9 @@ def main():
 
     elif args.mode == 'add_part':
         add_part(args.category, args.value, args.qty)
+
+    elif args.mode == 'add_parts':
+        add_parts(args.file)
 
 
 def add_pedal(name, file):
@@ -81,6 +84,18 @@ def add_part(category, value, qty):
     }
 
     parttools.save(part)
+
+def add_parts(file_name):
+    with open(file_name, 'r') as parts_file:
+        raw_parts = parts_file.readlines()
+
+    parts = (
+        parttools.parse(raw_part)
+        for raw_part in raw_parts
+    )
+
+    for part in parts:
+        parttools.save(part)
 
 
 if __name__ == '__main__':
